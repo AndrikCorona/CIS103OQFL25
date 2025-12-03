c7c7684c1df6149785c7e9f0badb9288bca879a6
AndrikCorona <acorona70@student.ccc.edu>
2025-12-03 14:56:49 -0600

    Add files via upload

---
+++ P2.py
@@ -0,0 +1,14 @@
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+rectangle_area = width * height
+print(f"The area of the rectangle is: {rectangle_area}")
+
+base = float(input("n\Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+triangle_area = 0.5 * base * height
+print(f"The area of the triangle is {triangle_area}")
+
+radius = float(input("\nEnter the radius of the circle: "))
+pi = 3.1459
+circle_area = pi * (radius ** 2 )
+print(f"The area of the circle is: {circle_area}")
